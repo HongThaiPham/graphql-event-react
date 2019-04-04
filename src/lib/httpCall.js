f6f43085504b85dda3c6917899d7fb9070f3ccc4
Pham Hong Thai <leopham@Phams-Mac-mini.local>
2019-04-04 16:04:27 +0700

    #13

---
+++ src/lib/httpCall.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "http://localhost:3000/graphql",
+  timeout: 1000,
+  headers: { "Content-Type": "application/json" }
+});
+
+export default instance;
--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,7 +1,83 @@
-import React from "react";
-
+import React, { useState } from "react";
+import "./Auth.scss";
+import httpCall from "../lib/httpCall";
 const AuthPage = () => {
-  return <h1>auth</h1>;
+  const [email, setEmail] = useState("test2@gmail.com");
+  const [password, setPassword] = useState("tester");
+  const [isLogin, setLogin] = useState(true);
+
+  const submitHandler = async e => {
+    e.preventDefault();
+    console.log({ email, password });
+    if (email.trim().length === 0 || password.trim().length === 0) {
+      return;
+    }
+    let requestBody = {
+      query: `
+        query {
+          login(email: "${email}", password: "${password}") {
+            userId
+            token
+            tokenExp
+          }
+        }
+      `
+    };
+    if (!isLogin) {
+      requestBody = {
+        query: `
+          mutation {
+            createUser(userInput: { email: "${email}", password: "${password}" }) {
+              _id
+              email
+            }
+          }
+        `
+      };
+    }
+    try {
+      const result = await httpCall.post("", JSON.stringify(requestBody));
+      if (result.status !== 200 && result.status !== 201) {
+        throw new Error("Failed!");
+      }
+      console.log(result);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const swithModeHandler = () => {
+    setLogin(!isLogin);
+  };
+
+  return (
+    <form className="auth-form" onSubmit={submitHandler}>
+      <div className="form-control">
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          name="email"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+        />
+      </div>
+      <div className="form-control">
+        <label htmlFor="email">Password</label>
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+        />
+      </div>
+      <div className="form-actions">
+        <button type="submit">{!isLogin ? "Signup" : "Login"}</button>
+        <button type="button" onClick={swithModeHandler}>
+          Switch to {isLogin ? "Signup" : "Login"}
+        </button>
+      </div>
+    </form>
+  );
 };
 
 export default AuthPage;
