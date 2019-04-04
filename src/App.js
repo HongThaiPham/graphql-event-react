80c6797b3dda4cf1ec2ff2fcab24779681347c28
Pham Hong Thai <leopham@Phams-Mac-mini.local>
2019-04-04 15:16:19 +0700

    #12

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 
-import "./App.css";
+import "./App.scss";
 
 import AuthPage from "./pages/Auth";
 import EventsPAge from "./pages/Events";
 import BookingPage from "./pages/Booking";
+
+import MainNavigation from "./components/Navigation/MainNavigation";
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Switch>
-          <Redirect from="/" to="/auth" exact />
-          <Route path="/auth" component={AuthPage} />
-          <Route path="/events" component={EventsPAge} />
-          <Route path="/bookings" component={BookingPage} />
-        </Switch>
+        <React.Fragment>
+          <MainNavigation />
+          <main className="main-content">
+            <Switch>
+              <Redirect from="/" to="/auth" exact />
+              <Route path="/auth" component={AuthPage} />
+              <Route path="/events" component={EventsPAge} />
+              <Route path="/bookings" component={BookingPage} />
+            </Switch>
+          </main>
+        </React.Fragment>
       </BrowserRouter>
     );
   }
